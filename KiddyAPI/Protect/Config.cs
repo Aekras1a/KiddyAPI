8e80ae9df3b25951ec53115d54a97201b8141df1
Falweek <falweekpm@gmail.com>
2019-05-14 14:28:50 +0300

    Добавил Config класс, для удобства работы

---
+++ KiddyAPI/Protect/Config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiddyAPI.Protect
+{
+    /// <summary>
+    /// Конфигурация различных модулей
+    /// </summary>
+    class Config
+    {
+        /// <summary>
+        /// Поле, которое вернет true, если программа запущена под администратором, иначе false
+        /// </summary>
+        public static bool IsAdmin
+        {
+            get { return WindowsIdentity.GetCurrent().Owner.IsWellKnown(WellKnownSidType.AccountAdministratorSid); }
+        }
+    }
+}
